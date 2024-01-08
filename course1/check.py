97482cbd11035163b4dce8fa63ef5f786d2eccd6
davy_chen <davy_chen@epam.com>
2024-01-08 21:11:03 +0800

    course 2

--- course1/check.py
+++ course1/check.py
@@ -3,7 +3,7 @@ from fastai.vision.core import PILImage
 
 learn = load_learner('course1/export.pkl')
 
-im = PILImage.create('course1/forest.jpg')
+im = PILImage.create('course1/bird2.png')
 print(im)
 is_bird,_,probs = learn.predict(im)
 print(f"This is a: {is_bird}.")
---
+++ course2/dogorcat.py
@@ -0,0 +1,19 @@
+import os
+os.environ['TORCH_HOME'] = 'course2'
+from fastai.vision.all import *
+
+defaults.use_cuda = False
+
+path = untar_data(URLs.PETS, dest=Path.cwd())/'images'
+
+def is_cat(x): return x[0].isupper() 
+
+dls = ImageDataLoaders.from_name_func('.',
+    get_image_files(path), valid_pct=0.2, seed=42,
+    label_func=is_cat,
+    item_tfms=Resize(192))
+
+learn = vision_learner(dls, resnet18, metrics=error_rate)
+learn.fine_tune(3)
+
+learn.export('course2/model.pkl')
